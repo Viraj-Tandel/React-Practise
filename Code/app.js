3f0a6cf9b9f0f42d3f20c77780428b277ba59876
its_viru03 <viru03@developer.com>
2023-06-03 14:56:24 +0530

    Basics of JSX, Functional Component

--- Code/app.js
+++ Code/app.js
@@ -1,14 +1,90 @@
-import React  from "react";
-import  ReactDOM  from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reactLogo from "./assets/r.png";
+import profile from "./assets/profile.png";
+import "../Code/index.css"
+// import x from './assets/sample'
 
-const subHeading1 = React.createElement('li',{ style: {color: 'green'}, id:'l1'}, "Item 1");
-const subHeading2 = React.createElement('li',{ style: {color: 'green'}, id:'l2'}, 'Item 2');
-const subHeading3 = React.createElement('li',{ style: {color: 'green'}, id:'l3'}, 'Item 3');
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const mainHeading = React.createElement("ul",{ style: { "color": 'red', "listStyleType": "none"}}, "Namste React!", [subHeading1,subHeading2]);
+// * using create React.createElement
+// const h1 = React.createElement('h1',{className: 'heading1',key:1},"Heading 1");
+// const h2 = React.createElement('h2',{className: 'heading2',key:2},"Heading 2");
+// const h3 = React.createElement('h3',{className: 'heading2',key:3},"Heading 3");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(mainHeading)
-const root2 = ReactDOM.createRoot(document.getElementById('root2'));
-root2.render(mainHeading);
- 
+// const div = React.createElement("div", { className: 'title'},[h1,h2,h3]);
+// root.render(div);
+
+// * using JSX
+// const nestedHeader = (
+//   <div className="title">
+//     <h1>Heading 1</h1>
+//     <h2>Heading 2</h2>
+//     <h3>Heading 3</h3>
+//   </div>
+// );
+// root.render(nestedHeader);
+
+// * using functional component
+// const NestedHeader = () => {
+//   return (
+//     <div className="title">
+//       <h1>Heading 1</h1>
+//       <h2>Heading 2</h2>
+//       <h3>Heading 3</h3>
+//     </div>
+//   );
+// };
+// root.render(<NestedHeader />);
+
+// * composition of component
+// const NestedHeader = () => {
+//   return (
+//     <div className="title">
+//       <h1>Heading 1</h1>
+//       <h2>Heading 2</h2>
+//       <SubComponent />
+//       {/* {SubComponent()} */}
+//       <h3>Heading 3</h3>
+//     </div>
+//   );
+// };
+
+// const SubComponent = () => (
+//     <h1>I am sub component which is injected in this.</h1>
+// )
+
+// root.render(<NestedHeader />)
+
+// * Header Component using functions
+
+
+const Logo = () => {
+  return (
+    <div className="company-logo">
+      <img src={reactLogo} alt="react logo" />
+    </div>
+  );
+};
+
+const SearchBar = () => (
+  <div className="searchBar">
+    <input placeholder="search here..." />
+  </div>
+);
+
+const UserIcon = () => (
+  <div className="user-icon">
+    <img src={profile} alt="profile logo"/>
+  </div>
+);
+
+const Header = () => (
+  <div className="header-wrapper">
+    <Logo />
+    <SearchBar />
+    <UserIcon />
+  </div>
+);
+
+root.render(<Header />);
